0ef0d881d230ae14cf12a0897d6c47ba051879f7
saranron <saranron@gmail.com>
2018-05-08 21:47:53 +0900

    test App componentDidMount

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -5,9 +5,34 @@ import Map from './components/Map';
 import MovieTable from './components/MovieTable';
 
 describe('<App />', () => {
+  let wrapper;
+  const movies = [
+    { title: 'title1' },
+    { title: 'title2' },
+  ];
+
+  beforeAll(() => {
+    const fetchPromise = Promise.resolve({
+      json: () => Promise.resolve(movies),
+    });
+    window.fetch = () => fetchPromise;
+  });
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
   it('renders without crashing', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find(Map)).toExist();
     expect(wrapper.find(MovieTable)).toExist();
+  });
+
+  it('should call api on componentDidMount', async () => {
+    const expected = movies.map(movie => expect.objectContaining(movie));
+
+    await wrapper.instance().componentDidMount();
+    wrapper.update();
+
+    expect(wrapper).toHaveState({ movies: expected });
   });
 });
